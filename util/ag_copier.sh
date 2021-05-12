73989ed86b87079e585c719d24aa5c7897b3ca46
Diego Krupitza <info@diegokrupitza.com>
2021-05-12 12:42:06 +0200

    Extended the ag copy script so you can choose between copying to codea or codeb

--- util/ag_copier.sh
+++ util/ag_copier.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Only run this command in the ag folder.
+COPYFOLDER="invalid"
 
+# Only run this command in the ag folder.
 if [[ "$PWD" == *ag ]] 
 then 
     echo "[✅] Exectuing the command in the right folder!";
@@ -10,13 +11,25 @@ else
     exit 1;
 fi
 
+if [[ "$1" == codea ]] 
+then
+    COPYFOLDER="codea"
+elif [[ "$1" == codeb ]] 
+then
+    COPYFOLDER="codeb"
+else
+    echo "[❌] You need to specify if you want to copy to codea or codeb...";
+    exit 1;
+fi  
+
+
 
 for file in *; do   
     
     if [[ $file == *\.3 ]] # * is used for pattern matching
     then
-        echo "[✅] Copying $file to ../codeb/ag_test_case_${file}";
-        cp "$file" "../codeb/ag_test_case_${file}"; 
+        echo "[✅] Copying $file to ../${COPYFOLDER}/ag_test_case_${file}";
+        cp "$file" "../${COPYFOLDER}/ag_test_case_${file}"; 
     fi
     
 done
